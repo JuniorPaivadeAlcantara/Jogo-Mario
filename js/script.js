2432acda1fdd88b46b15237f393f208c05399add
JuniorPaivadeAlcantara <142550329+JuniorPaivadeAlcantara@users.noreply.github.com>
2023-10-29 17:18:33 -0300

    implementa game over

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const cloud = document.querySelector('.cloud');
+const gameOver = document.querySelector('.game-over');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -13,9 +15,13 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition =  pipe.offsetLeft;
+    const cloudPosition = cloud.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        cloud.style.animation = 'none';
+        cloud.style.left = `${cloudPosition}px`;
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -24,8 +30,11 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = '../images/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        gameOver.style.visibility = 'visible';
+
 
         clearInterval(loop);
     }
@@ -33,3 +42,5 @@ const loop = setInterval(() => {
 }, 10);
 
 document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
+document.addEventListener('mousedown', jump);
